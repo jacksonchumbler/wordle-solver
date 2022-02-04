da352320d5aeb8f47e77c898ded082007b2c3945
jacksonchumbler <48119889+jacksonchumbler@users.noreply.github.com>
2022-02-04 02:06:07 -0500

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -10,7 +10,6 @@ def suggest_word(candidates):
     guess = input("What was your guess (e to quit)")
     correct_indx = []
     wrong_spot_indx = []
-    #while(guess != "e"):
     num_correct = int(input("How many did you get in the right spot? "))
     if(num_correct > 0):
         for i in range(num_correct):
@@ -24,9 +23,8 @@ def suggest_word(candidates):
     guesses.append([guess, correct_indx, wrong_spot_indx])
     correct_indx = []
     wrong_spot_indx = []
-    #guess = input("What was your guess (e to quit)")
-    #Now, we have all guesses and correctness
 
+    #Now, we have all guesses and correctness...
     #Whittle all words without correct index
     to_remove = []
     for word in candidates:
@@ -39,7 +37,7 @@ def suggest_word(candidates):
                         to_remove.append(word)
                     removed = True
                     break
-            #Wrong Spot
+            #Wrong Spot, Valid Letter
             if removed:
                 break
             for i in guess[2]:
@@ -51,17 +49,12 @@ def suggest_word(candidates):
                     if word not in to_remove:
                         to_remove.append(word)
                     break
+
             #Incorrect Letters
-            
             incorrect_letters = ['0','1','2','3','4']
             incorrect_letters = [item for item in incorrect_letters if item not in guess[1]]
             incorrect_letters = [item for item in incorrect_letters if item not in guess[2]]
 
-                    
-            #if word == "truce":
-                #print(guess[1])
-                #print(guess[2])
-                #print(incorrect_letters)
             for i in incorrect_letters:
                 if guess[0][int(i)] in word:
                     if word not in to_remove:
@@ -82,4 +75,3 @@ while(user_inp != "e"):
     print(candidates)
     user_inp = input("Add Another? (e to quit)")
 print(candidates)
-
